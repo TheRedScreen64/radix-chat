6f6881b674e54760d3a627059a386ef6ea36fe2e
Noah Nagel <itznosehad@gmail.com>
2024-02-24 12:25:31 +0100

    working on content management + added transitions between pages

---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+<h1>Radix Chat - Frontend solution</h1>
+<h2>Config</h2>
+<ul>
+<li><strong>assets/json/pages-config.json</strong>  meta tag information</li>
+<li><strong>properties.h</strong> application settings</li>
+<ul>
--- frontend/html/build.sh
+++ frontend/html/build.sh
@@ -7,16 +7,29 @@
 # -w: ignore warnings
 
 mkdir -p assets
+mkdir -p assets/scripts
 
 # login screen
 gcc  -w -E -x c -P html/pages/login/index.html -o assets/login.nosehad
+gcc  -w -E -x c -P html/pages/login/login.js -o assets/scripts/login.js
+
 # landing page
 gcc  -w -E -x c -P html/pages/landing/index.html -o assets/landing.nosehad
+
 # frame
 gcc  -w -E -x c -P html/frame.h -o assets/frame.nosehad
+
 # loader 
 gcc  -w -E -x c -P html/loader/index.html -o assets/loader.nosehad
+
 # 404 page 
 gcc  -w -E -x c -P html/404.html -o assets/404.nosehad
+
 # background image 
 gcc  -w -E -x c -P html/background.svg -o assets/images/background.svg
+
+# content managment
+gcc  -w -E -x c -P html/generic/Content/cms.html -o assets/cms.nosehad
+
+# message utility
+gcc  -w -E -x c -P html/generic/message.js -o assets/scripts/message.js
